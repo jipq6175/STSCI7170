f878bdf752c4fed3c3be246067ecab7f7b37af2c
jipq6175 <yc2253@cornell.edu>
2019-10-26 13:40:44 -0400

    hw3

--- hw3/pr2.r
+++ hw3/pr2.r
@@ -1,9 +1,9 @@
 
 library('lme4');
 
-n = 4;
+n = 5;
 d = 2;
-t = 5;
+t = 4;
 
 Y = c(1.08, 1.99, 1.46, 1.21, 1.48, 2.50, 2.62, 1.95,
       1.19, 2.10, 1.21, 0.96, 0.62, 0.88, 0.68, 0.48,
@@ -28,4 +28,20 @@ A5 = Jn %x% Jd %x% Ct
 A6 = Cn %x% Jd %x% Ct
 A7 = Jn %x% Cd %x% Ct
 A8 = Cn %x% Cd %x% Ct
+
+
+
+serum =  data.frame(matrix(ncol = 4, nrow = n*d*t))
+colnames(serum) = c("response", "subject", "drug", "time")
+serum$response = Y
+serum$subject = 1:n %x% rep(1, d) %x% rep(1,t)
+serum$drug = rep(1, n) %x% 1:d %x% rep(1,t)
+serum$time = rep(1, n) %x% rep(1, d) %x% 1:t
+
+ml = lmer(response ~ 1 + (1|subject)
+                       + (drug)
+                       + (time)
+                       + (drug|subject) 
+                       + (time|subject)
+                       + (drug|time), data=serum, REML=FALSE)
 
