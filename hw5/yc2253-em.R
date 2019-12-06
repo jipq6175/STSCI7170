a66aaf9d061e023976edff8d9df50ccc3ef2aa1f
jipq6175 <yc2253@cornell.edu>
2019-12-05 22:51:13 -0500

    em

--- hw5/yc2253-em.R
+++ hw5/yc2253-em.R
@@ -1,13 +1,68 @@
 
+
+library(MASS)
+
 # The EM function 
 
 
-vcem = function(tol=1e-5){
+vcem = function(X, Y, Z, beta, u, tol=1e-5){
   
-  for(i in 1:5){
-    print(tol);
+  # Check the dimensions 
+  n = dim(X)[1]; 
+  p = dim(X)[2]; 
+  q = dim(Z)[2];
+
+  if (length(Y) != n){
+    print('The rows of X does not equal to length of Y .. '); 
+    return(0); 
   }
-  #print(tol);
+  
+  if (dim(Z)[1] != n){
+    print('The rows of Z does not equal to length of Y .. '); 
+    return(0); 
+  }
+  
+  if (length(beta) != p){
+    print('The length of beta does not equal to columns of X .. '); 
+    return(0); 
+  }
+  
+  if (length(beta) != q+1){
+    print('The length of u does not equal to columns of Z .. '); 
+    return(0); 
+  }
+  
+  
+  
+  # Some variables
+  d = 1.0; # difference in log-likelihood
+  beta0 = beta; # The beta container
+  u0 = u; # The u container
+  P = X %*% ginv(t(X) %*% X) %*% t(X); # Projection matrix
+  z = cbind(rep(1.0, n), Z);
+  
+  while(d > tol){
+    
+    sigma = diag(u0) %*% Z %*% t(Z); # Calculate sigma
+    invsigma = ginv(sigma);
+    
+    # Calculate random effects update
+    for(i in 1:length(u0)){
+      
+      b = invsigma %*% (Y - X %*% beta0);
+      v = t(Z[, i]) %*% b;
+      u0[i] = u0[i] - u0[i]^2 * (t(Z[, i]) %*% invsigma %*% Z[, i]) + u0[i]^2 * t(v) %*% v; 
+    }
+    
+    # Calculate fixed effect update
+    s = X %*% beta0 + u0[1] * b;
+    beta0 = solve(X, P %*% s);
+    
+    # Calculate the log-likelihood
+    
+    
+    
+  }
   
   
   
